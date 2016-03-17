a92f36ad42ac900040c8dabf2fb8fc4c0b83878d
cliu <fluency.03@gmail.com>
2016-03-17 14:26:01 +0100

    init logsequence-rnn.py

---
+++ logsequence-rnn.py
@@ -0,0 +1,17 @@
+"""
+This program
+
+Author: Chang Liu (fluency03)
+Data: 2016-03-17
+"""
+
+
+class LogSequenceAnalyzer(object):
+    """
+    A log sequence analyzer.
+    """
+    def __init__(self, sequence_file):
+        """
+        Inits LogSequenceAnalysis class.
+        """
+        self.sequence_file = sequence_file
