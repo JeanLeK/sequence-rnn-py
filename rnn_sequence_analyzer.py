b1640195fa277d55c95c4776191e98efda3a2183
cliu <fluency.03@gmail.com>
2016-04-02 18:31:29 +0200

    merge build_lstm and build_gru

--- rnn_sequence_analyzer.py
+++ rnn_sequence_analyzer.py
@@ -42,12 +42,15 @@ class SequenceAnalyzer(object):
         self.output_len = output_len
         self.model = Sequential()
 
-    def build_lstm(self, mapping='o2o', nb_layers=2, dropout=0.2):
+    def build(self, layer='LSTM', mapping='o2o', nb_layers=2, dropout=0.2):
         """
-        Stacked LSTM with specified dropout rate (default 0.2), built with
+        Stacked RNN with specified dropout rate (default 0.2), built with
         softmax activation, cross entropy loss and rmsprop optimizer.
 
         Arguments:
+            layer: {string}, the type of the layers in the RNN Model
+                'LSTM': LSTM layers
+                'GRU': GRU layers
             mapping: {string}, input to output mapping
                 'o2o': one-to-one
                 'm2m': many-to-many
@@ -55,6 +58,20 @@ class SequenceAnalyzer(object):
             dropout: {float}, dropout value
         """
         print "Building Model..."
+
+        # check the layer type: LSTM or GRU
+        if layer == 'LSTM':
+            class LAYER(LSTM):
+                """
+                LAYER as LSTM.
+                """
+                pass
+        elif layer == 'GRU':
+            class LAYER(GRU):
+                """
+                LAYER as GRU.
+                """
+                pass
 
         # check whether the last layer return sequences
         if mapping == 'o2o':
@@ -64,10 +81,10 @@ class SequenceAnalyzer(object):
             # if mapping is many-to-many
             return_sequences = True
 
-        # 2 layer LSTM with specified number of nodes in the hidden layer.
-        self.model.add(LSTM(self.hidden_len, return_sequences=True,
-                            input_shape=(self.sentence_length,
-                                         self.input_len)))
+        # 2 layer RNN layers with specified number of nodes in the hidden layer.
+        self.model.add(LAYER(self.hidden_len, return_sequences=True,
+                             input_shape=(self.sentence_length,
+                                          self.input_len)))
         self.model.add(Dropout(dropout))
 
         for nl in range(nb_layers-1):
@@ -77,58 +94,8 @@ class SequenceAnalyzer(object):
             else:
                 return_sequences_ = return_sequences
             # build hidden layers
-            self.model.add(LSTM(self.hidden_len,
-                                return_sequences=return_sequences_))
-            self.model.add(Dropout(dropout))
-
-        if mapping == 'o2o':
-            # if mapping is one-to-one
-            self.model.add(Dense(self.output_len))
-        elif mapping == 'm2m':
-            # if mapping is many-to-many
-            self.model.add(TimeDistributedDense(self.output_len))
-
-        self.model.add(Activation('softmax'))
-
-        self.model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
-
-    def build_gru(self, mapping='o2o', nb_layers=2, dropout=0.2):
-        """
-        Stacked GRU with specified dropout rate (default 0.2), built with
-        softmax activation, cross entropy loss and rmsprop optimizer.
-
-        Arguments:
-            mapping: {string}, input to output mapping
-                'o2o': one-to-one
-                'm2m': many-to-many
-            nb_layers: {integer}, number of layers in total
-            dropout: {float}, dropout value
-        """
-        print "Building Model..."
-
-        # check whether the last layer return sequences
-        if mapping == 'o2o':
-            # if mapping is one-to-one
-            return_sequences = False
-        elif mapping == 'm2m':
-            # if mapping is many-to-many
-            return_sequences = True
-
-        # 2 layer GRU with specified number of nodes in the hidden layer.
-        self.model.add(GRU(self.hidden_len, return_sequences=True,
-                           input_shape=(self.sentence_length,
-                                        self.input_len)))
-        self.model.add(Dropout(dropout))
-
-        for nl in range(nb_layers-1):
-            # check whether return sequences
-            if nl != nb_layers-2:
-                return_sequences_ = True
-            else:
-                return_sequences_ = return_sequences
-            # build hidden layers
-            self.model.add(GRU(self.hidden_len,
-                               return_sequences=return_sequences_))
+            self.model.add(LAYER(self.hidden_len,
+                                 return_sequences=return_sequences_))
             self.model.add(Dropout(dropout))
 
         if mapping == 'o2o':
@@ -345,7 +312,7 @@ def train(hidden_len=512, batch_size=128, nb_epoch=1, validation_split=0.1,
     rnn = SequenceAnalyzer(sentence_length, input_len, hidden_len, input_len)
 
     # build model
-    rnn.build_lstm(mapping=mapping)
+    rnn.build(layer='LSTM', mapping=mapping)
 
     # load the previous model weights
     # rnn.load_model("weights4.hdf5")
